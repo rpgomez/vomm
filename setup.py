5e35de5cbc7c58b5fc0429bcb18161d76261cfde
Rod Gomez <armyofthepenguin@gmail.com>
2016-08-06 21:26:15 -0400

    Added setup.py and MANIFEST.in
    I need these to make a proper python package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='vomm',
+      version='0.1',
+      description='Implements 2 algorithms for variable order markov models',
+      author='Rod Gomez',
+      author_email='armyofthe penguin@gmail.com',
+      url='https://github.com',
+      py_modules=['vomm'],
+     )
