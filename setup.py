cee4c339b5fd6b51d4e9c3f0584ef252924c0acb
Rod Gomez <armyofthepenguin@gmail.com>
2016-08-08 09:23:27 -0400

    Modified setup.py.
    Corrected the url keyword value to point at the github project.
    
    Added the install_requires keyword for the benefit of the setuptools
    to also install numpy as a required dependency.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='vomm',
       description='Implements 2 algorithms for variable order markov models',
       author='Rod Gomez',
       author_email='armyofthe penguin@gmail.com',
-      url='https://github.com',
+      url='https://github.com/rpgomez/vomm',
+      install_requires=['numpy',],
       py_modules=['vomm'],
      )
